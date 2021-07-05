dce30b9f1b88d02605b843294e9343fa38b59164
Krishnol Sen <77930884+krishnol-sen@users.noreply.github.com>
2021-07-06 01:04:19 +0530

    Uploaded a python file

---
+++ test.py
@@ -0,0 +1,12 @@
+# adding a file via upload
+
+n = int(input("Enter a number : "));
+
+for i in range(1, n+1) :
+    for j in range(1, i+1) :
+        print("*", end = " ")
+    print()
+
+a = input("\nPress enter to exit !")
+
+# This basic python file creates a pattern.
